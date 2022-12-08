4cdd5e3a12e06e35a69c7ed94bad28af0d5d0dc0
Ketleen Anne <ketleen@ice.ufjf.br>
2022-12-08 14:08:11 -0300

    add service student

---
+++ src/main/java/service/ServiceStudent.java
@@ -0,0 +1,32 @@
+package service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import model.Student;
+import repository.RepositoryStudent;
+
+@Service
+public class ServiceStudent {
+    
+    @Autowired
+    private RepositoryStudent repositoryStudent;
+    
+    public List<Student> findAllStudents() {
+        return repositoryStudent.findAll();
+    }
+    
+    public void saveStudent(Student student) {
+        repositoryStudent.save(student);
+    }
+
+    public Student findStudentById(Long id) {
+        return repositoryStudent.findById(id).orElse(null);
+    }
+
+    public void deleteStudent(Long id) {
+        repositoryStudent.deleteById(id);
+    }
+}
